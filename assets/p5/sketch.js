3de61633e72c63727f601da791c2724688005578
annakleger <anna.kleger@stud.hslu.ch>
2022-05-18 11:06:29 +0200

    update 18.05

--- assets/p5/sketch.js
+++ assets/p5/sketch.js
@@ -1,9 +1,34 @@
+let loadedJSON = null;
+let test;
+let gesichtskoordinatex;
+let gesichtskoordinatey;
 var grid_unit = 50;
 
+
+function preload() {
+
+
+}
+
 function setup() {
+
     //Canvas erstellen und einmitten abhängig von der window-Grösse; müsste noch automatischen Refresh bei canvasResized programmieren
     var coordinate_canvas = createCanvas(700, 700);
     coordinate_canvas.position(windowWidth / 2 - 350, windowHeight / 2 - 350);
+
+    textSize(36);
+    loadBtn = createButton("Load JSON from file");
+    loadBtn.position(30, 1000);
+    loadBtn.mousePressed(loadJSONFile);
+}
+
+function loadJSONFile() {
+    // Load the JSON from file
+    loadedJSON = loadJSON('test.json', onFileload);
+}
+
+function onFileload() {
+    console.log("success");
 
 }
 
@@ -25,6 +50,9 @@ function draw() {
     //Beispielkreis; orange
     fill(10, 80, 80, 0.5);
     circle(425, 425, 50);
+
+    fill(60, 100, 100, 0.5);
+    circle(test.gesichtskoordinatex, test.gesichtskoordinatey, 50);
 
 
 }
