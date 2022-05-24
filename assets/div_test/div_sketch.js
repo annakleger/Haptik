3a7cc65d5b67a6082b0ca5de8bd1d7971ab76687
celine-schmid <celine.schmid@stud.hslu.ch>
2022-05-24 10:23:52 +0200

    hanna, patterns

--- assets/div_test/div_sketch.js
+++ assets/div_test/div_sketch.js
@@ -4,7 +4,9 @@ var grid_unit = 50;
 
 let col = 14
 let row = 14
-let cell_size = 0
+let cell_size = 0;
+
+let patterns = new Array();
 
 // This function loads resources that will be used later.
 function preload() {
@@ -14,26 +16,26 @@ function preload() {
     png1 = loadImage('../img/4Zeichenfläche 1.png');
 
 
-    pattern_1 = loadImage('../img/patterns_01.png');
-    pattern_2 = loadImage('../img/patterns-02.png');
-    pattern_3 = loadImage('../img/patterns-03.png');
-    pattern_4 = loadImage('../img/patterns-04.png');
-    pattern_5 = loadImage('../img/patterns-05.png');
-    pattern_6 = loadImage('../img/patterns-06.png');
-    pattern_7 = loadImage('../img/patterns-07.png');
-    pattern_8 = loadImage('../img/patterns-08.png');
-    pattern_9 = loadImage('../img/patterns-09.png');
-    pattern_10 = loadImage('../img/patterns-10.png');
-    pattern_11 = loadImage('../img/patterns-11.png');
-    pattern_12 = loadImage('../img/patterns-12.png');
-    pattern_13 = loadImage('../img/patterns-13.png');
-    pattern_14 = loadImage('../img/patterns-14.png');
-    pattern_15 = loadImage('../img/patterns-15.png');
-    pattern_16 = loadImage('../img/patterns-16.png');
-    pattern_17 = loadImage('../img/patterns-17.png');
-    pattern_18 = loadImage('../img/patterns-18.png');
-    pattern_19 = loadImage('../img/patterns-19.png');
-    pattern_20 = loadImage('../img/patterns-20.png');
+    patterns[0] = loadImage('../img/patterns-01.png');
+    patterns[1] = loadImage('../img/patterns-02.png');
+    patterns[2] = loadImage('../img/patterns-03.png');
+    patterns[3] = loadImage('../img/patterns-04.png');
+    patterns[4] = loadImage('../img/patterns-05.png');
+    patterns[5] = loadImage('../img/patterns-06.png');
+    patterns[6] = loadImage('../img/patterns-07.png');
+    patterns[7] = loadImage('../img/patterns-08.png');
+    patterns[8] = loadImage('../img/patterns-09.png');
+    patterns[9] = loadImage('../img/patterns-10.png');
+    patterns[10] = loadImage('../img/patterns-11.png');
+    patterns[11] = loadImage('../img/patterns-12.png');
+    patterns[12] = loadImage('../img/patterns-13.png');
+    patterns[13] = loadImage('../img/patterns-14.png');
+    patterns[14] = loadImage('../img/patterns-15.png');
+    patterns[15] = loadImage('../img/patterns-16.png');
+    patterns[16] = loadImage('../img/patterns-17.png');
+    patterns[17] = loadImage('../img/patterns-18.png');
+    patterns[18] = loadImage('../img/patterns-19.png');
+    patterns[19] = loadImage('../img/patterns-20.png');
 }
 
 //Alles was nur einmal, beim ersten Laden der Seite ausgeführt werden soll
@@ -49,7 +51,7 @@ function setup() {
     save_button.position(40, 990);
     save_button.mousePressed(saveSketch);
 
-
+    angleMode(DEGREES);
     // //hatching
     // var hatchingNumber = 2; //wie viele hatchings
 
@@ -84,9 +86,9 @@ function get_positions_face(face_data) {
 function get_hand(hand_data) {
 
     let hand = hand_data["hand"]
-    if (typeof hand != String) {
-        return hand
-    }
+        /* if (typeof hand != String) {
+             return hand
+         }*/
     return hand.split(", ")
 }
 
@@ -133,7 +135,7 @@ function grid(value) {
 function show_gesture(value) {
     let face_positions = get_positions_face(test_data[value])
     let hand = get_hand(test_data[value])
-        // console.log(face_positions)
+        // console.log(hand);
     for (let x = 0; x <= col; x += 1) {
         for (let y = 0; y <= row; y += 1) {
             // check the position of the face
@@ -143,8 +145,30 @@ function show_gesture(value) {
                 let pos_x = element.x
                 let pos_y = element.y
                 if (x == pos_x && y == pos_y) {
-                    fill(100, 100, 255, 70)
-                    square(x * cell_size, y * cell_size, cell_size)
+                    let bildnummer;
+                    let bild;
+                    let welcheHand;
+                    for (let h = 0; h < hand.length; h++) {
+                        bildnummer = hand[h].substr(1, hand[h].length);
+                        welcheHand = hand[h].substr(0, 1);
+                        bild = patterns[bildnummer - 1];
+
+                        if (welcheHand == "R") {
+                            push();
+                            translate(x * cell_size + cell_size / 2, y * cell_size + cell_size / 2);
+                            rotate(90);
+                            image(bild, -cell_size / 2, -cell_size / 2, cell_size, cell_size);
+                            pop();
+                        } else {
+                            image(bild, x * cell_size, y * cell_size, cell_size, cell_size);
+                        }
+
+                        //console.log(bild);
+                    }
+
+
+                    //fill(100, 100, 255, 70)
+                    //square(x * cell_size, y * cell_size, cell_size)
                 }
             }
 
